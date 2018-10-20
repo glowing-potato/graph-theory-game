197ce25486f20351f0aa9f4b5cdba43347cae2ca
Zach Deibert <zachdeibert@gmail.com>
2018-10-20 09:35:40 -0500

    Implemented basic LevelsMenu

--- src/menus/LevelsMenu.js
+++ src/menus/LevelsMenu.js
@@ -1,10 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import background from "./background.svg";
+import "./LevelsMenu.css";
+
+const levels = [
+    { "id": "euler-walk", "name": "Euler Walks" },
+    { "id": "hamilton-walk", "name": "Hamiltonian Walks" }
+];
 
 export default class LevelsMenu extends React.Component {
     render() {
         return (
-            <div>
-                Levels Menu
+            <div className="levels-menu">
+                <h1>Select Game Mode</h1>
+                <ul>
+                    {levels.map((level, i) => (
+                        <li key={i}>
+                            <Link to={`/${level.id}/`}>{level.name}</Link>
+                        </li>
+                    ))}
+                </ul>
+                <Link to="/">Back to Main Menu</Link>
+                <img src={background} alt="" className="background" />
             </div>
         );
     }
