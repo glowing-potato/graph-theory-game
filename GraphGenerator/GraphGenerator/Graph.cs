92a2f7bfa3705a631d30da22d9ce6d3c7129cd55
Nathan Gawith <NathanGawith@gmail.com>
2018-10-20 09:15:37 -0500

    Added base for Graph Generator Project

---
+++ GraphGenerator/GraphGenerator/Graph.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphGenerator
+{
+    public class Graph
+    {
+        private class Vertex
+        {
+            private double x;
+            private double y;
+        }
+
+        private int order;
+        private Vertex[] vertices;
+        private bool[][] adjMatrix;
+
+        public Graph(int order, bool[][] adjMatrix)
+        {
+            this.order = order;
+            this.adjMatrix = adjMatrix;
+        }
+
+        public void AddEdge(int vertex1, int vertex2)
+        {
+
+        }
+
+        public void RemoveEdge(int vertex1, int vertex2)
+        {
+
+        }
+
+        public void AddVertex()
+        {
+
+        }
+
+        public void RemoveVertex(int position)
+        {
+
+        }
+
+        public bool IsHamiltonian()
+        {
+            return false;
+        }
+
+        public bool IsEulerian()
+        {
+            return false;
+        }
+
+        public bool IsIsomorphicTo(Graph g)
+        {
+            return false;
+        }
+
+        public int Order { get => order; }
+
+    }
+}
---
+++ GraphGenerator/GraphGenerator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphGenerator
+{
+
+    class GenerateAdgacencyMatricies
+    {
+        class Convert
+        {
+            // outputs JSON objects representing graphs
+            //  origionally generated as matricies
+            public static void ToJSON()
+            {
+
+            }
+            // outputs Zach's preferred output of lists
+            //  of verticies [(x, y)] and edges [(v, v)]
+            public static void ToVertEdge()
+            {
+
+            }
+        }
+        public GenerateAdgacencyMatricies()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            GenerateAdgacencyMatricies gam = new GenerateAdgacencyMatricies();
+        }
+    }
+}
