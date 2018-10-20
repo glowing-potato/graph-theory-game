dfc8c9ccff86d42605980688482c52a260e79311
Zach Deibert <zachdeibert@gmail.com>
2018-10-20 08:06:27 -0500

    Updated game naming

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import MainMenu from "./menus/MainMenu";
 import LevelMenu from "./menus/LevelMenu";
-import EulerCycle from "./games/EulerCycle";
-import HamiltonianCycle from "./games/HamiltonianCycle";
+import EulerWalk from "./games/EulerWalk";
+import HamiltonWalk from "./games/HamiltonWalk";
 
 export default class App extends React.Component {
     render() {
         return (
             <BrowserRouter>
                 <Switch>
-                    <Route path="/euler/:level" component={EulerCycle} />
-                    <Route path="/hamiltonian/:level" component={HamiltonianCycle} />
+                    <Route path="/euler-walk/:level" component={EulerWalk} />
+                    <Route path="/hamilton-walk/:level" component={HamiltonWalk} />
                     <Route path="/:type" component={LevelMenu} />
                     <Route path="/" component={MainMenu} />
                 </Switch>
