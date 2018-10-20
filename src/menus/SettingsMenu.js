70f5e0c92eb2779a9311eb58beec4c004d79ebf1
Zach Deibert <zachdeibert@gmail.com>
2018-10-20 09:23:13 -0500

    Created a skeleton of settings menu

--- src/menus/SettingsMenu.js
+++ src/menus/SettingsMenu.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import background from "./background.svg";
+import "./SettingsMenu.css";
 
 export default class SettingsMenu extends React.Component {
     render() {
         return (
-            <div>
-                Levels Menu
+            <div className="settings-menu">
+                <h1>Settings</h1>
+                <p>
+                    TODO
+                </p>
+                <Link to="/">Back to Main Menu</Link>
+                <img src={background} alt="" className="background" />
             </div>
         );
     }
