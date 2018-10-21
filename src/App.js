8494978d669663c093c2bf0c99043a0e8c42e7af
Zach Deibert <zachdeibert@gmail.com>
2018-10-21 02:43:29 -0500

    Added Euler Cycle and Hamilton Circuit game modes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import LevelsMenu from "./menus/LevelsMenu";
 import SettingsMenu from "./menus/SettingsMenu";
 import EulerTrail from "./games/EulerTrail";
 import HamiltonPath from "./games/HamiltonPath";
+import EulerCycle from "./games/EulerCycle";
+import HamiltonCircuit from "./games/HamiltonCircuit";
 
 export default class App extends React.Component {
     render() {
@@ -15,6 +17,8 @@ export default class App extends React.Component {
                 <Switch>
                     <Route path="/euler-trail/:level" component={EulerTrail} />
                     <Route path="/hamilton-path/:level" component={HamiltonPath} />
+                    <Route path="/euler-cycle/:level" component={EulerCycle} />
+                    <Route path="/hamilton-circuit/:level" component={HamiltonCircuit} />
                     <Route path="/about" component={AboutMenu} />
                     <Route path="/levels" component={LevelsMenu} />
                     <Route path="/settings" component={SettingsMenu} />
---
+++ src/games/HamiltonCircuit.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Game from "../components/Game";
+import TraceableGraph from "../components/TraceableGraph";
+import data from "./HamiltonCircuit.json";
+
+export default class HamiltonCircuit extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            "won": false,
+            "nonce": -1
+        };
+        this.evaluateWin = this.evaluateWin.bind(this);
+        this.handleReload = this.handleReload.bind(this);
+    }
+
+    componentDidUpdate(lastProps) {
+        if (lastProps.match !== this.props.match) {
+            this.setState({
+                "won": false
+            });
+        }
+    }
+
+    evaluateWin(history) {
+        if (history.length === data[this.props.match.params.level - 1].v.length + 1 && history[0].v[0] === history[history.length - 1].v[0] && history[0].v[1] === history[history.length - 1].v[1]) {
+            this.setState({
+                "won": true
+            });
+        }
+    }
+
+    handleReload(nonce) {
+        this.setState({
+            "nonce": nonce,
+            "won": false
+        });
+    }
+
+    render() {
+        return (
+            <Game won={this.state.won} onReload={this.handleReload} levels={data.length}>
+                <TraceableGraph verts={data[this.props.match.params.level - 1].v} edges={data[this.props.match.params.level - 1].e}
+                    onGraphChange={this.evaluateWin} nonce={this.state.nonce} />
+            </Game>
+        );
+    }
+}
